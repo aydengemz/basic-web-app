4f8741e723dbfdadc2f05ee2f0f6462833b32358
Ayden Xu <aydenxunc@gmail.com>
2025-02-24 11:10:54 -0500

    hallo

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Ayden";
+  }
+
+  if (query.toLowerCase().includes("andrew id")) {
+    return "Your Andrew ID is: aydenx"; 
   }
 
   return "";
